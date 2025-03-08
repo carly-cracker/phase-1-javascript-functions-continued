6241d0a312109a12ac3f4f99ec53981c30529f71
Korir Carlos <carlkorir226@gmail.com>
2025-03-08 12:31:06 +0300

    adds func 2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(toDo='roller-skate'){
+    return `This Saturday, I want to ${toDo}!`
+}
+
+function mondayWork (duties = 'go to the office'){
+    return `This Monday, I will ${duties}.`
+}
+
+function wrapAdjective(symbol){
+    return (function (ngori="special"){
+        return `You are ${symbol}${ngori}${symbol}!` 
+    })
+}
